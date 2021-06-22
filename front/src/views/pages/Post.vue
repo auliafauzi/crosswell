e59b531ef28d2aa70b0d3bbc5efeaf926b1e0ce7
aulia.fauzi <aulia.fauzi@ottodigital.id>
2021-06-22 23:54:23 +0700

    dynamic button for different user and user_role

--- front/src/views/pages/Post.vue
+++ front/src/views/pages/Post.vue
@@ -1,12 +1,13 @@
 <template>
 	<div class="page-layout-sidebar-right scrollable only-y">
-		<div class="flex">
-			<div class="box grow card-base card-shadow--small p-24" style= "margin-bottom: 10px; background: rgba(95, 143, 223, 0.2);">
+		<!-- <div class="flex"> -->
+			<div class="box grow card-base card-shadow--small p-24" style= "margin-bottom: 20px; margin-left: 50px; margin-right: 300px; background: rgba(95, 143, 223, 0.2);">
 				<!-- <div style=" max-width: 1100px; margin: 0 auto; "> -->
-				<div style=" max-width: 1100px;">
-          <!-- <p style="font-size: 10px;">
-            {{i.user_id}}
-          <p> -->
+				<!-- <div style=" max-width: 11000px;"> -->
+        <div>
+          <p style="font-size: 10px;">
+            {{this.content.user_id}}
+          <p>
 					<!-- <p class="mt-18">
 						<img src="@/assets/images/photo1.jpg" class="demo-img" alt="demo image">
 					</p> -->
@@ -17,13 +18,39 @@
 
 				</div>
 			</div>
-			<!-- <div class="sidebar scrollable" :class="{'open':sidebarOpen}">
-				<el-button size="small" class="close-btn" @click="sidebarOpen = false">close</el-button>
-				<ul>
-					<li v-for="i in 20" :key="i">Sidebar Item {{i}}</li>
-				</ul>
-			</div> -->
-		</div>
+      <div flex>
+        <b-button variant="outline-primary" size="sm" class="mr-1" style="margin-left: 70px;">
+          Leave Comment
+        </b-button>
+          <b-button variant="outline-primary" size="sm" class="mr-1" style="margin-left: 10px;" v-if= "user_id == this.content.user_id || this.user_role == 1">
+            Edit Post
+          </b-button>
+          <b-button variant="outline-danger" size="sm" class="mr-1" style="margin-left: 10px;" v-if= "user_id == this.content.user_id || this.user_role == 1">
+            Delete Post
+          </b-button>
+      </div>
+      <div class="box grow card-base card-shadow--small p-24" style= "margin-bottom: 10px; margin-top: 10px; margin-left: 70px; margin-right: 300px;" v-for="i in commentList">
+				<div style=" max-width: 1100px;">
+          <p style="font-size: 10px;">
+            {{i.user_id}}
+          <p>
+          <p style="font-size: 10px;">
+            {{i.date}}
+          <p>
+					<p class="mt-0">
+						{{i.comment}}
+					</p>
+				</div>
+        <div flex>
+          <b-button variant="outline-primary" size="sm" class="mr-1" v-if= "user_id == i.user_id || user_role == 1">
+            Edit Comment
+          </b-button>
+          <b-button variant="outline-danger" size="sm" class="mr-1" v-if= "user_id == i.user_id || user_role == 1">
+            Delete Comment
+          </b-button>
+        </div>
+			</div>
+		<!-- </div> -->
 
     <b-modal id="NewPostModal"
        v-model="postModalToggle"
@@ -41,9 +68,15 @@ export default {
 	data() {
 		return {
 			list : ['test1', 'test2', 'test3'],
+      user_id : "002",
+      // dummy hard coded user_role
+      user_role : 2,
+      //
       content : [],
+      commentList : [],
       content_id : '',
       postModalToggle : false,
+      tango : true,
 			sidebarOpen: false
 		}
 	},
@@ -65,9 +98,6 @@ export default {
   },
   methods : {
     getData() {
-      // console.log("yaa1")
-      // console.log("yaa" + this.$route.params.id)
-      // this.token = getToken()
       var headers = {
         'Content-Type': 'application/json',
         // 'Authorization': `Bearer ${this.token}`
@@ -84,13 +114,24 @@ export default {
               setTimeout(() => {this.errorMessage= '',console.log(response.data.message),window.location.href = this.HomeLocation}, 2000);
             } else {
               this.content = response.data.content
-                // this.dataCompany = response.data.data.user;
-                // this.TotalData = Number(response.data.data.total)
-                console.log(this.contentList)
-                console.log(this.contentList[0].title)
             }
           }
         })
+        axios.get(`http://127.0.0.1:5000/api/v1/commentList/${this.$route.params.id}`,{
+              headers
+              },
+          ).then(response => {
+            if (response.status === 200) {
+              if (response.data.code === 401) {
+                this.errorAlert = true;
+                this.errorMessage = response.data.message;
+                setTimeout(() => {this.errorMessage= '',console.log(response.data.message),window.location.href = this.HomeLocation}, 2000);
+              } else {
+                this.commentList = response.data.comment
+              }
+            }
+          })
+
     },
     goTo(content_id) {
       // this.postModalToggle = true
