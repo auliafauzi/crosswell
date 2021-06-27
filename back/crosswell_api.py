8151c59e28566d90ea3a246ed286436755fbdbe5
aulia.fauzi <aulia.fauzi@ottodigital.id>
2021-06-28 03:29:04 +0700

    post comment function

--- back/crosswell_api.py
+++ back/crosswell_api.py
@@ -44,7 +44,7 @@ def api_test():
 @app.route('/api/v1/contentList', methods= ['GET'])
 def contentList():
 	result = []
-	contentList = util.connectToPostgres('get',config['targethostname'],config['targetdatabase'],config['targetusername'],None,None,util.query('contentList',None))
+	contentList = util.connectToPostgres('get',config['targethostname'],config['targetdatabase'],config['targetusername'],None,None,util.query('contentList',None, None))
 	for i in contentList :
 		zipped = dict(zip(util.data_structure['content'], i))
 		result.append(zipped)
@@ -52,15 +52,25 @@ def contentList():
 
 @app.route('/api/v1/contentDetail/<string:content_id>', methods= ['GET'])
 def contentDetail(content_id):
-	content_result = util.connectToPostgres('get',config['targethostname'],config['targetdatabase'],config['targetusername'],None,None,util.query('getContent',content_id))
+	content_result = util.connectToPostgres('get',config['targethostname'],config['targetdatabase'],config['targetusername'],None,None,util.query('getContent',content_id, None))
 	content_result = dict(zip(util.data_structure['content'], content_result[0]))
 	return jsonify({"content":content_result,"code":200})
+
+@app.route('/api/v1/contentImage/<string:content_id>', methods= ['GET'])
+def contentImage(content_id):
+	try :
+		image_path = util.connectToPostgres('get',config['targethostname'],config['targetdatabase'],config['targetusername'],None,None,util.query('get_image_path',content_id,None))
+		# content_result = dict(zip(util.data_structure['content'], content_result[0]))
+		# return jsonify({"image":image_path,"code":200})
+		return flask.send_file(image_path[0][0], mimetype='image/jpeg/jpg/png', as_attachment=True)
+	except : 
+		return jsonify({"message":"failed to get image","code":"x01"})
 
 
 @app.route('/api/v1/commentList/<string:parent_id>', methods= ['GET'])
 def commentList(parent_id):
 	comment_list = []
-	comment_result = util.connectToPostgres('get',config['targethostname'],config['targetdatabase'],config['targetusername'],None,None,util.query('commentList',parent_id))
+	comment_result = util.connectToPostgres('get',config['targethostname'],config['targetdatabase'],config['targetusername'],None,None,util.query('commentList',parent_id, None))
 	for i in comment_result :
 		zipped = dict(zip(util.data_structure['comment'], i))
 		comment_list.append(zipped)
@@ -69,11 +79,11 @@ def commentList(parent_id):
 
 @app.route('/api/v1/postContent/<string:user_id>', methods= ['POST'])
 def postContet(user_id):
-	methods1 = 'get'
-	query1 = "select content_id from cms.content"
+	# methods1 = 'get'
+	# query1 = "select content_id from cms.content"
 	# maxnum = util.getMax(util.connectToPostgres(methods1,config['targethostname'],config['targetdatabase'],config['targetusername'],None,None,query1))
 	# new_content_id = "p" + str(int(maxnum[0])+1)
-	new_content_id = "p" + str(util.connectToPostgres('get',config['targethostname'],config['targetdatabase'],config['targetusername'],None,None,util.query('next_id',None))[0][0])
+	new_content_id = "p" + str(util.connectToPostgres('get',config['targethostname'],config['targetdatabase'],config['targetusername'],None,None,util.query('next_id','content_id','content'))[0][0])
 	###
 	context = request.get_data()
 	title = request.form['title']
@@ -82,18 +92,36 @@ def postContet(user_id):
 	latitude = request.form['latitude']
 	try :
 		image = request.files['image']
+		filename = image.filename
+		image_path = os.path.join("uploads", image.filename)
+		image.save(image_path)
 	except :
+		image_path = ''
 		image = ''
 	try :
 		file = request.files['file']
+		filename = file.filename
+		file_path = os.path.join("uploads", file.filename)
+		file.save(file_path)
 	except :
+		file_path = ''
 		file = ''
-	filename = image.filename
-	image.save(os.path.join("uploads", image.filename))
-	new_content = [new_content_id,title,content,user_id,longitude,latitude,None,None,datetime.now().strftime("%Y-%m-%d %H:%M:%S")]
+	new_content = [new_content_id,title,content,user_id,longitude,latitude,image_path,file_path,datetime.now().strftime("%Y-%m-%d %H:%M:%S")]
 	query2 = util.insertQuery("cms.content", new_content)
-	result_push = util.connectToPostgres("push",config['targethostname'],config['targetdatabase'],config['targetusername'],None,new_content,query2)
+	result_push = util.connectToPostgres("push",config['targethostname'],config['targetdatabase'],config['targetusername'],None,new_content,util.insertQuery("cms.content", new_content))
 	return jsonify({"new_content_id":new_content_id,"title":title,"content":content,"new_content":new_content,"result_push":result_push,"code":200})
+
+@app.route('/api/v1/postComment/<string:user_id>/<string:parent_id>', methods= ['POST'])
+def postComment(user_id, parent_id):
+	# context = request.get_data()
+	# comment = request.form['comment']
+	comment = request.json['comment']
+	print(comment)
+	new_coment_id = "c" + str(util.connectToPostgres('get',config['targethostname'],config['targetdatabase'],config['targetusername'],None,None,util.query('next_id','comment_id','comment'))[0][0])
+	new_comment = [new_coment_id, comment, user_id, parent_id,datetime.now().strftime("%Y-%m-%d %H:%M:%S")]
+	result_push = util.connectToPostgres("push",config['targethostname'],config['targetdatabase'],config['targetusername'],None,new_comment,util.insertQuery("cms.comment", new_comment))
+	return jsonify({"new_coment_id":new_coment_id,"comment":comment,"new_comment":new_comment,"result_push":result_push,"code":200})
+
 
 
 app.run()
