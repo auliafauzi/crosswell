244a2da2938a42b55357057f2124b70c13ed5a52
aulia.fauzi <aulia.fauzi@ottodigital.id>
2021-06-28 03:29:22 +0700

    post comment function(2)

--- back/util.py
+++ back/util.py
@@ -3,19 +3,23 @@ import psycopg2
 import re
 
 data_structure = {
-	'content' : ['content_id', 'title', 'content', 'user_id','long','lat','image_path','file_path', 'date'],
+	'content' : ['content_id', 'title', 'content', 'user_id','long','lat','image_path','file_path', 'date', 'count_comment'],
 	'comment' : ['comment_id', 'comment', 'user_id', 'parent_id','date']
 } 
 
-def query(param1, param2) :
+def query(param1, param2,param3) :
 	if param1 == "contentList" :
-		 return "select content_id, title, concat(left(content, 350), '...'), user_id, long, lat, image_path, file_path , date from cms.content order by date desc;"
+		 return "select content_id, title, concat(left(content, 350), '...'), cont.user_id, long, lat, image_path, file_path , cont.date, com.counting from cms.content cont left join (select parent_id , count(comment_id) as counting from cms.comment group by parent_id) as com on cont.content_id = com.parent_id  order by date desc;"
 	elif param1 == "getContent" :
 		return "select content_id, title, content, user_id, long, lat, image_path, file_path, date  from cms.content where content_id ='"+param2+"' order by date desc"
 	elif param1 == 'commentList' :
 		return "select * from cms.comment where parent_id ='"+param2+"' order by date asc"
 	elif param1 == 'next_id' :
-		return "select max(NULLIF(regexp_replace(content_id, '\D','','g'), '')::numeric) +1 from cms.content"
+		return "select max(NULLIF(regexp_replace("+param2+", '\D','','g'), '')::numeric) +1 from cms."+param3
+	elif param1 == 'count_comment' :
+		return "select count(comment_id) from cms.comment where parent_id = " + param2
+	elif param1 == 'get_image_path' :
+		return "select image_path from cms.content where content_id = '" + param2 + "'"
 	else :
 		return
 
